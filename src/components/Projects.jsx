bc5c8f4a15349e1c1e20b762d065b707d086a37b
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-02-16 21:01:41 +0530

    commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import SectionHead from './SectionHead'
 import Button from './Button'
 import ProjectCards from './ProjectCards'
 import Sample1 from '../assets/images/Sample1.webp'
+import Sample2 from '../assets/images/sample2.webp'
 function Projects() {
   const ProjectData = [
     {
@@ -11,7 +12,14 @@ function Projects() {
       alt: "This image contains sain Portfolio",
       paragragh: "Sain Porfolio is a sample Website project which is created with HTML, Tailwind CSS , CSS and Javacript.",
       link: "https://lucky-banoffee-4e4a8c.netlify.app/"
-    }
+    },
+    {
+      title: "DP Sports World",
+      src: Sample2,
+      alt: "This image contains Dp Sports World",
+      paragragh: "DP Sports World is a sports academy sample Website project which is created with HTML, Tailwind CSS , CSS and Javacript.",
+      link: "https://lucky-banoffee-4e4a8c.netlify.app/"
+    },
   ]
 
 
